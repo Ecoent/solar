28ee182435f78fb29bd995e99ac0dad830751c24
marcel <marcel@wolfgang-ebert.de>
2019-10-04 16:35:44 +0200

    Use Section instead of VerticalLayout in DialogBody

--- src/components/Dialog/DialogBody.tsx
+++ src/components/Dialog/DialogBody.tsx
@@ -4,6 +4,7 @@ import { useIsMobile, RefStateObject } from "../../hooks/userinterface"
 import { MobileKeyboardOpenedSelector } from "../../theme"
 import ErrorBoundary from "../ErrorBoundary"
 import { Box, VerticalLayout } from "../Layout/Box"
+import { Section } from "../Layout/Page"
 
 const isRefStateObject = (thing: any): thing is RefStateObject =>
   thing && "element" in thing && typeof thing.update === "function"
@@ -45,6 +46,7 @@ const topStyle: React.CSSProperties = {
 interface Props {
   actions?: React.ReactNode | RefStateObject
   actionsPosition?: "after-content" | "bottom"
+  brandColored?: boolean
   background?: React.ReactNode
   children: React.ReactNode
   excessWidth?: number
@@ -86,15 +88,16 @@ function DialogBody(props: Props) {
 
   return (
     <ErrorBoundary>
-      <VerticalLayout
-        className="dialog-body"
+      <Section
+        brandColored={props.brandColored}
         width="100%"
         height="100%"
         maxWidth={900}
         alignItems="stretch"
         overflowX="hidden"
-        padding={isSmallScreen ? "12px 24px" : " 24px 32px"}
+        padding={isSmallScreen ? "12px 24px" : "24px 32px"}
         margin="0 auto"
+        top
       >
         {topContent}
         {background}
@@ -112,7 +115,7 @@ function DialogBody(props: Props) {
           {actionsPosition === "after-content" ? actionsContent : null}
         </VerticalLayout>
         {actionsPosition === "bottom" ? actionsContent : null}
-      </VerticalLayout>
+      </Section>
     </ErrorBoundary>
   )
 }

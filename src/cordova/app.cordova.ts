ec41c4de36ddbe87fd7f9fe30d571c1b915b8993
Marcel Ebert <marcel@wolfgang-ebert.de>
2019-11-06 12:04:00 +0100

    Create implementations for the updating mechanism

--- src/cordova/app.cordova.ts
+++ src/cordova/app.cordova.ts
@@ -10,6 +10,7 @@ import initializeQRReader from "./qr-reader"
 import { getCurrentSettings, initSecureStorage, storeKeys, initKeyStore } from "./storage"
 import { bioAuthenticate, isBiometricAuthAvailable } from "./bio-auth"
 import { registerURLHandler } from "./protocol-handler"
+import { registerUpdateHandler } from "./updater"
 
 const iframe = document.getElementById("walletframe") as HTMLIFrameElement
 const showSplashScreenOnIOS = () => (process.env.PLATFORM === "ios" ? navigator.splashscreen.show() : undefined)
@@ -55,6 +56,7 @@ function onDeviceReady() {
   }
 
   registerURLHandler(contentWindow, iframeReady)
+  registerUpdateHandler()
   initializeQRReader()
   initializeClipboard(cordova)
   initializeIPhoneNotchFix()
